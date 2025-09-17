fc214c506f757c9e702c8e6a75018eeb27bd4a69
Aponi James <Jamesjulius176@gmail.com>
2025-09-17 16:33:27 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,25 +1,10 @@
 
 import type { NavLink, Service, TeamMember } from './types';
-import { Sun, Building } from 'lucide-react';
+import { Sun, Building, Zap } from 'lucide-react';
 
-const ElectricalServiceIcon = (props: any) => (
-    <svg
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <path d="M4 14a1 1 0 0 1-.78-1.63l9.9-10.2a.5.5 0 0 1 .86.46l-1.92 6.02A1 1 0 0 0 13 10h7a1 1 0 0 1 .78 1.63l-9.9 10.2a.5.5 0 0 1-.86-.46l1.92-6.02A1 1 0 0 0 11 14z" />
-    </svg>
-);
+const ElectricalServiceIcon = Zap;
 
-const GeneralContractingIcon = (props: any) => (
-  // eslint-disable-next-line @next/next/no-img-element
-  <img src="https://cdn-icons-png.flaticon.com/512/3098/3098409.png" alt="General Contracting" {...props} />
-);
+const GeneralContractingIcon = Building;
 
 export const navLinks: NavLink[] = [
   { href: '/', label: 'Home' },
