94167673038571ae556290362431583de3d28df5
Aponi James <Jamesjulius176@gmail.com>
2025-09-17 16:10:26 +0000

    https://goodwill-electrical.co.uk/wp-content/uploads/2025/04/commercial-

--- next.config.ts
+++ next.config.ts
@@ -46,6 +46,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'goodwill-electrical.co.uk',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
